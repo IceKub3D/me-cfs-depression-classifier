6a257410bc3aeed1ae16c7374180ee54f103be09
IceKub3D <boen911@gmail.com>
2025-07-09 09:27:37 +0300

    Remove redundant test_script.py and test_data.py

--- scripts/test_data.py
+++
@@ -1,4 +0,0 @@
-import pandas as pd
-test_df = pd.read_csv('data/test_data.csv')
-print(test_df[['gender', 'work_status', 'social_activity_level', 'exercise_frequency', 'meditation_or_mindfulness']].head())
-print(test_df.dtypes)
